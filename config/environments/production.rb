3737a85aff4df8da4b24764f7d5bcc430e7bbd06
uncl3_b3n <bjlinville1@gmail.com>
2018-01-02 18:23:55 -0500

    changed config so assets won't precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,4 +104,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #don't precompile
+  config.assets.initialize_on_precompile = false
 end
